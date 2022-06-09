76f4a401d78b9de1bc42828568fb8967d2abb3b4
yasinalibd02 <yasinalibd02@gmail.com>
2022-06-09 10:10:54 +0600

    grofish

---
+++ lib/data/categories_data.dart
@@ -0,0 +1,15 @@
+import 'package:grofresh/models/categories_model.dart';
+
+List <categoriesModel> categorie=[
+
+
+  categoriesModel(image: "assets/image/breakfast.jpg", text: "Breakfast"),
+  categoriesModel(image: "assets/image/breakfast.jpg", text: "Breakfast"),
+  categoriesModel(image: "assets/image/breakfast.jpg", text: "Breakfast"),
+  categoriesModel(image: "assets/image/breakfast.jpg", text: "Breakfast"),
+  categoriesModel(image: "assets/image/breakfast.jpg", text: "Breakfast"),
+  categoriesModel(image: "assets/image/breakfast.jpg", text: "Breakfast"),
+  //categoriesModel(image: "4+", text: "View All"),
+  
+]
+;
